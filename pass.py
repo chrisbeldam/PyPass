996e2b382423a80ec84a1b6428e79d91ec712ff3
Chris Beldam <chrisgbeldam@gmail.com>
2018-02-06 20:19:27 +0000

    Password assignment

--- pass.py
+++ pass.py
@@ -3,17 +3,19 @@ from string import ascii_uppercase, digits
 
 # ask user if they want to generate a new password
 def generate_password():
-    new_pass_gen = input("Do you wish to generate a new password?(Y/N): ").upper()
+    new_pass_gen = input("Do you wish to generate a password?(Y/N): ").upper()
     if new_pass_gen == "Y":
         # find out the length of the password
         new_pass_length = input("What length combination of letters and digits would you like your password to be?: ")
         # generate a random combination of letters based on their specified length
         new_password = ''.join(choice(ascii_uppercase + digits) for i in range(int(new_pass_length)))
         print("Your new password is:" + " " + new_password)
+
+        assignment = input("Please type in what you wish to assign this password to: ").lower()
         # ask if they want to save the password to a text file
         save_to_file = input("Do you wish to save this to a text file?(Y/N): ").upper()
         if save_to_file == "Y":
-            write_password_to_file(new_password)
+            write_password_to_file(new_password, assignment)
             print("Your password has been saved to : " + "passwords.txt")
         view_passwords = input("Do you want to view your current passwords?(Y/N): ").upper()
         if view_passwords == "Y":
@@ -22,16 +24,15 @@ def generate_password():
         if more_passwords == "Y":
             generate_password()
     else:
-        print("Here are you current passwords")
         view_current_passwords()
 
-def write_password_to_file(new_password):
+def write_password_to_file(new_password, assignment):
     with open('passwords.txt', 'w') as password:
-        password.write(new_password + '\n')
+        password.write(new_password + '-' + assignment + '\n')
 
 def view_current_passwords():
-    file = open('passwords.text', 'r')
+    file = open('passwords.txt', 'r')
+    print("Here is a list of your current passwords" + "\n")
     print(file.read())
-    pass
 
 generate_password()
